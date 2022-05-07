cf0ac79db4bbec77a1ada204540363151e82ec1e
M4yfl0w3r <mr.mayflower15@gmail.com>
2022-05-07 11:19:32 +0200

    add heap tests

--- tst/Heap_Test.cpp
+++ tst/Heap_Test.cpp
@@ -10,6 +10,13 @@ TEST(Constructor, OneArgumentConstructor)
   EXPECT_EQ(heap.get_capacity(), 40);
 }
 
+TEST(Constructor, InitListConstructor)
+{
+  Flower_Max_Heap heap {40, 20, 30, 50};
+
+  EXPECT_EQ(heap.get_size(), 4);
+}
+
 class HeapTest: public ::testing::Test 
 {
 
@@ -17,10 +24,12 @@ protected:
   void SetUp() override
   {
     heap1 = {0, 1, 2, 3, 4, 5};
+    heap_to_sort = {5, 9, 2, 1, 8, 17, 3};
   }
 
   Flower_Max_Heap heap1;
   Flower_Max_Heap heap2;
+  Flower_Max_Heap heap_to_sort;
 };
 
 TEST_F(HeapTest, IsEmpty)
@@ -32,4 +41,28 @@ TEST_F(HeapTest, IsEmpty)
 TEST_F(HeapTest, GetSize)
 {
   EXPECT_EQ(6, heap1.get_size());
+  EXPECT_EQ(0, heap2.get_size());
 }
+
+TEST_F(HeapTest, GetMax)
+{
+  EXPECT_EQ(5, heap1.flower_extract_max());
+}
+
+TEST_F(HeapTest, HeapSort)
+{
+  std::vector<int> sorted = {17, 9, 8, 5, 3, 2, 1};
+
+  EXPECT_TRUE(sorted == heap_to_sort.flower_heap_sort());
+}
+
+TEST_F(HeapTest, DISABLED_Searching)
+{
+  // EXPECT_EQ(heap1.flower_search(3), );
+}
+
+TEST_F(HeapTest, DISABLED_GetItem)
+{
+  // EXPECT_EQ(heap1.flower_get_item(3), );
+}
+
